a951e3fa5d6363398df0e8770e57ee25895f4ba0
Brian Velez <bvez94@gmail.com>
2019-05-16 13:12:25 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Dog 
+
+    #ruby setter 
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def bark
+        puts "woof!"
+    end
+
+    #ruby getter 
+    def name
+        @this_dogs_name
+    end
+
+end
